84c9bf6ba02d3a5d16c579ac6289d769c5ee839d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2022-03-12 17:03:05 -0800

    updated login ui

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
   return (
     <>
       {
-      window.location.pathname !== "/" ||
+      window.location.pathname !== "/" &&
       window.location.pathname !== "/sign-up" ? 
       (<HeaderBar/>) : null}
       {/* <div> */}
--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,40 +1,62 @@
-import React from "react";
-import {Formik}  from 'formik';
-import { Form } from "react-bootstrap";
+import React from 'react';
+import { Formik } from 'formik';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import './Login.css';
 
 const Login = () => {
   return (
-      <div>
-        <Formik
-          initialValues={{
-            username: "",
-            password: "",
-        }}>
-          <Form>
-            <Form.Group>
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                name="username"
-                // value={values.username}
-                // onChange={handleChange}
-                required
-              />
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Password"
-                name="password"
-                // value={values.username}
-                // onChange={handleChange}
-                required
-              />
-            </Form.Group>
-          </Form>
-        </Formik>
+    <div id='login-container'>
+      <div id='login-bg'>
+        <div id='login-card'>
+          <Formik
+            initialValues={{
+              username: '',
+              password: ''
+            }}
+          >
+            <Form>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Username'
+                  name='username'
+                  // value={values.username}
+                  // onChange={handleChange}
+                  required
+                />
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type='text'
+                  placeholder='Password'
+                  name='password'
+                  // value={values.username}
+                  // onChange={handleChange}
+                  required
+                />
+              </Form.Group>
+
+              <br />
+              <Button
+                variant='primary'
+                type='submit'
+                className='custom-primary-button'
+              >
+                Login
+              </Button>
+              <br />
+
+              <Link to='/signup'>
+                <p id='login-signup-link'>Create an Account</p>
+              </Link>
+            </Form>
+          </Formik>
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Login;
